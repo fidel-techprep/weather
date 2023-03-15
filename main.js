72d3a593b0908bc38c32f220d12c0eba21be1836
fidel-techprep <127764786+fidel-techprep@users.noreply.github.com>
2023-03-15 11:51:38 -0500

    add button CSS, minor code corrections
    Added a button :active effect

--- main.js
+++ main.js
@@ -21,7 +21,8 @@ function setQuery(evt) {
 }
 
 /* Fetch Chicago data for the initial weather values */
-window.addEventListener('load', getResults('q=Chicago'));
+window.addEventListener('load', () => getResults('q=Chicago'));
+
 
 /* Make API request and handle errors */
 function getResults (query) {
@@ -44,38 +45,39 @@ function getResults (query) {
 }
 
 /* Populate weather data on the page */
+let city = document.querySelector('.location .city');
+let now = new Date();
+let date = document.querySelector('.location .date');
+let temp = document.querySelector('.current .temp');
+let weather_el = document.querySelector('.current .weather');
+let hilow = document.querySelector('.hi-low');
+let hilowCelsius = document.querySelector('.hi-low-celsius');
+let humidity = document.querySelector('.humidity');
+let feels = document.querySelector('.feels-like');
+let celsius = document.querySelector('.celsius');
+
 function displayResults (weather) {
     
     if (weather) {
         
         searchbox.value='';
 
-        let city = document.querySelector('.location .city');
         city.innerText = `${weather.name}, ${weather.sys.country}`;
-
-        let now = new Date();
-        let date = document.querySelector('.location .date');
+        
         date.innerText = dateBuilder(now);
-        
-        let temp = document.querySelector('.current .temp');
+               
         temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`
-        
-        let weather_el = document.querySelector('.current .weather');
+       
         weather_el.innerText = weather.weather[0].main;
 
-        let hilow = document.querySelector('.hi-low');
         hilow.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F`;
 
-        let hilowCelsius = document.querySelector('.hi-low-celsius');
         hilowCelsius.innerText = `${Math.round((weather.main.temp_min - 32) * 0.5556)}°C / ${Math.round((weather.main.temp_max - 32) * 0.5556)}°C`;
         
-        let humidity = document.querySelector('.humidity');
         humidity.innerText = `Humidity ${weather.main.humidity}%`;
         
-        let feels = document.querySelector('.feels-like');
         feels.innerText = `Feels Like ${Math.round(weather.main.feels_like)}°F`;
-
-        let celsius = document.querySelector('.celsius');
+        
         celsius.innerText = `${Math.round((weather.main.temp - 32) * 0.5556)}°C`;
     }
 }
@@ -96,13 +98,13 @@ function dateBuilder(d) {
 /* Handle errors from location functionality */
 function errorHandler() {
 
-        const link = document.createElement("a");
-        link.href = "";
-        link.classList.add('location-link');
-        link.innerHTML = 'try again';
-        document.querySelector('.location-option').innerHTML = `unable to determine your location`;
-        document.querySelector('header').appendChild(link);
-        link.addEventListener('click', () => {window.location = window.location});
+    const link = document.createElement("a");
+    link.href = "";
+    link.classList.add('location-link');
+    link.innerHTML = 'try again';
+    document.querySelector('.location-option').innerHTML = `unable to determine your location`;
+    document.querySelector('header').appendChild(link);
+    link.addEventListener('click', () => {window.location = window.location});
 }
 
 /* Determine device's location */
